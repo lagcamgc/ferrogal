8a5881a393a946da5a680f8f7bed07e621781cc3
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2019-05-14 16:51:16 -0400

    Fix de barra de precio

--- js/functions.js
+++ js/functions.js
@@ -213,41 +213,70 @@ function containerHeightSecond(size) {
 }
 
 function lowPriceBar () {
-    var minValue =Math.min(document.getElementById('minInput').value, $('#maxInput').attr('value')-1);
-    var max = document.getElementById('minInput').getAttribute("max");
-    var min = document.getElementById('minInput').getAttribute("min");
 
-    document.getElementById('min').value = minValue; 
-    styleLeft(min, max, minValue);
+    var minInput = document.getElementById('minInput').value;
+    var maxInput = $('#maxInput').attr('value');
+
+    console.log(minInput);
+    console.log(maxInput);
+    if (minInput <= maxInput) {
+        var minValue =Math.min(minInput, maxInput-1);
+        var max = document.getElementById('minInput').getAttribute("max");
+        var min = document.getElementById('minInput').getAttribute("min");
+
+        document.getElementById('min').value = minValue; 
+        styleLeft(min, max, minValue);
+    }
 }
 
 function highPriceBar () {
-    var maxValue =Math.max(document.getElementById('maxInput').value, $('#minInput').attr('value')-(-1));
 
-    var max = document.getElementById('maxInput').getAttribute("max");
-    var min = document.getElementById('maxInput').getAttribute("min");
+    var maxInput = document.getElementById('maxInput').value
+    var minInput = document.getElementById('minInput').value;
 
-    document.getElementById('max').value = maxValue;
+    if (maxInput >= minInput) {
+        var maxValue =Math.max(maxInput, minInput-(-1));
 
-    styleRigth(min, max, maxValue);
+        var max = document.getElementById('maxInput').getAttribute("max");
+        var min = document.getElementById('maxInput').getAttribute("min");
+
+        document.getElementById('max').value = maxValue;
+
+        styleRigth(min, max, maxValue);
+    }
 }
 
 function lowPriceInput() {
 
-    var minValue =Math.min(document.getElementById('min').value, $('#maxInput').attr('value')-1);
-    var max = document.getElementById('min').getAttribute("max");
-    var min = document.getElementById('min').getAttribute("min");
-    styleLeft(min, max, minValue);
+    var minInput = document.getElementById('min').value;
+    var maxInput = $('#maxInput').attr('value');
+    console.log(minInput);
+    console.log(maxInput);
+    if (minInput <= maxInput) {
+
+        var minValue =Math.min(minInput, maxInput);
+        var max = document.getElementById('min').getAttribute("max");
+        var min = document.getElementById('min').getAttribute("min");
+
+        styleLeft(min, max, minValue);
+    }
 
 }
 
 function highPriceInput () {
-    var maxValue =Math.max(document.getElementById('max').value, $('#minInput').attr('value')-(-1));
 
-    var max = document.getElementById('max').getAttribute("max");
-    var min = document.getElementById('max').getAttribute("min");
+    var maxInput = document.getElementById('max').value;
+    var minInput = document.getElementById('minInput').value;
+    console.log(minInput);
+    console.log(maxInput);
+    if (maxInput >= minInput) {
+        var maxValue =Math.max(maxInput, minInput-(-1));
 
-    styleRigth(min, max, maxValue);
+        var max = document.getElementById('max').getAttribute("max");
+        var min = document.getElementById('max').getAttribute("min");
+
+        styleRigth(min, max, maxValue);
+    }
 }
 
 function styleLeft (min, max, minValue) {
@@ -268,122 +297,3 @@ document.getElementById('myDropdown').addEventListener('click', function(event) 
     event.stopPropagation();
 });
 
-(function($) {
-    "use strict";  
-    
-    //* Form js
-    function verificationForm(){
-        //jQuery time
-        var current_fs, next_fs, previous_fs; //fieldsets
-        var left, opacity, scale; //fieldset properties which we will animate
-        var animating; //flag to prevent quick multi-click glitches
-
-        $(".next").click(function () {
-            if (animating) return false;
-            animating = true;
-
-            current_fs = $(this).parent();
-            next_fs = $(this).parent().next();
-
-            //activate next step on progressbar using the index of next_fs
-            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-            //show the next fieldset
-            next_fs.show();
-            //hide the current fieldset with style
-            current_fs.animate({
-                opacity: 0
-            }, {
-                step: function (now, mx) {
-                    //as the opacity of current_fs reduces to 0 - stored in "now"
-                    //1. scale current_fs down to 80%
-                    scale = 1 - (1 - now) * 0.2;
-                    //2. bring next_fs from the right(50%)
-                    left = (now * 50) + "%";
-                    //3. increase opacity of next_fs to 1 as it moves in
-                    opacity = 1 - now;
-                    current_fs.css({
-                        'transform': 'scale(' + scale + ')',
-                        'position': 'absolute'
-                    });
-                    next_fs.css({
-                        'left': left,
-                        'opacity': opacity
-                    });
-                },
-                duration: 800,
-                complete: function () {
-                    current_fs.hide();
-                    animating = false;
-                },
-                //this comes from the custom easing plugin
-                easing: 'easeInOutBack'
-            });
-        });
-
-        $(".previous").click(function () {
-            if (animating) return false;
-            animating = true;
-
-            current_fs = $(this).parent();
-            previous_fs = $(this).parent().prev();
-
-            //de-activate current step on progressbar
-            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-            //show the previous fieldset
-            previous_fs.show();
-            //hide the current fieldset with style
-            current_fs.animate({
-                opacity: 0
-            }, {
-                step: function (now, mx) {
-                    //as the opacity of current_fs reduces to 0 - stored in "now"
-                    //1. scale previous_fs from 80% to 100%
-                    scale = 0.8 + (1 - now) * 0.2;
-                    //2. take current_fs to the right(50%) - from 0%
-                    left = ((1 - now) * 50) + "%";
-                    //3. increase opacity of previous_fs to 1 as it moves in
-                    opacity = 1 - now;
-                    current_fs.css({
-                        'left': left
-                    });
-                    previous_fs.css({
-                        'transform': 'scale(' + scale + ')',
-                        'opacity': opacity
-                    });
-                },
-                duration: 800,
-                complete: function () {
-                    current_fs.hide();
-                    animating = false;
-                },
-                //this comes from the custom easing plugin
-                easing: 'easeInOutBack'
-            });
-        });
-
-        $(".submit").click(function () {
-            return false;
-        })
-    }; 
-    
-    //* Add Phone no select
-    function phoneNoselect(){
-        if ( $('#msform').length ){   
-            $("#phone").intlTelInput(); 
-            $("#phone").intlTelInput("setNumber", "+880"); 
-        };
-    }; 
-    //* Select js
-    function nice_Select(){
-        if ( $('.product_select').length ){ 
-            $('select').niceSelect();
-        };
-    }; 
-    /*Function Calls*/  
-    verificationForm ();
-    phoneNoselect ();
-    nice_Select ();
-
-})(jQuery); 
